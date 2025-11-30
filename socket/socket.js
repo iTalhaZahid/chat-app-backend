18d4aaff0f4e0546a4002585f6a7a72a90661ae4
Talha Zahid <italhazahid@gmail.com>
2025-11-30 20:59:06 +0500

    Socket Setup Completed

---
+++ socket/socket.js
@@ -0,0 +1,48 @@
+import dotenv from 'dotenv';
+import { Server as SocketIOServer, Socket } from 'socket.io';
+import jwt from 'jsonwebtoken';
+dotenv.config();
+
+export default function initializeSocketServer(server) {
+    //socket io instance
+    const io = new SocketIOServer(server, {
+        cors: {
+            origin: '*', // Allow all origins (you can restrict this in production)
+            methods: ['GET', 'POST']
+        }
+    });
+
+    //authentication middleware
+
+    io.use((socket, next) => {
+        const token = socket.handshake.auth.token;
+        if (!token) {
+            return next(new Error('Authentication error: No token provided'));
+        } else {
+            next(new Error('Authentication error'));
+        }
+
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                return next(new Error('Authentication error: Invalid token'));
+            }
+
+            //attach user data to socket object
+            let userData = decoded.user;
+            socket.data = userData;
+            socket.data.userId = userData.id;
+            next();
+        });
+    });
+
+    //handle socket connection
+    io.on('connection', (socket) => {
+        console.log(`User connected: ${socket.data.userId}`);
+    });
+
+    //handle socket disconnection
+    io.on('disconnect', (socket) => {
+        console.log(`User disconnected: ${socket.data.userId}`);
+    });
+    return io;
+}
