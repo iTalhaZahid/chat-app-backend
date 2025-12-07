bf8b482bc0dd93fe97978fb03af9ddb18b99501f
Talha Zahid <italhazahid@gmail.com>
2025-12-07 12:37:22 +0500

    Conversation Model

---
+++ models/Conversation.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const ConversationSchema = new Schema({
+
+    type:{
+        type: String,
+        enum: ['direct', 'group'],
+        required: true
+    },
+    name:String,
+    participants:[{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    }],
+    lastMessage:{
+        type: Schema.Types.ObjectId,
+        ref: "Message",
+    },
+
+    createdBy:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    avatar:{
+        type: String,
+        default: "",
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt:{
+        type: Date,
+        default: Date.now,
+    }
+});
+
+
+// Middleware to update 'updatedAt' before saving
+ConversationSchema.pre('save', function (next) {
+    this.updatedAt = Date.now();
+    next();
+});
+
+export default  model("Conversation", ConversationSchema);
