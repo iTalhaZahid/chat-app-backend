f7eecc023244e923f034b5366b85813fc9ff1978
Talha Zahid <italhazahid@gmail.com>
2025-12-08 23:57:19 +0500

    Comleted Conversation Model

--- models/Conversation.js
+++ models/Conversation.js
@@ -41,7 +41,7 @@ const ConversationSchema = new Schema({
 // Middleware to update 'updatedAt' before saving
 ConversationSchema.pre('save', function (next) {
     this.updatedAt = Date.now();
-    next();
+    // next();
 });
 
 export default  model("Conversation", ConversationSchema);
