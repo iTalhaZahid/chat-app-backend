0ca0e7665a395b01f7a9fd4dbee05f54bd895c95
Talha Zahid <italhazahid@gmail.com>
2025-12-14 13:00:30 +0500

    Added Routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { loginUser, registerUser, verifyEmail } from "../controllers/auth.controllers.js";
+import { checkOTP, loginUser, registerUser, requestResetPassword, resetPassword, verifyEmail } from "../controllers/auth.controllers.js";
 
-const router= Router();
+const router = Router();
 
-router.post("/register",registerUser);
-router.post("/login",loginUser);
-router.post("/verify-email",verifyEmail);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/verify-email", verifyEmail);
+router.post("/password/forgot", requestResetPassword);
+router.post("/password/otp", checkOTP);
+router.post("/password/reset", resetPassword);
 
 export default router;
