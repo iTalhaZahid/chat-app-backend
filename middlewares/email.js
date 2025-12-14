c7e2a2b1de1c061ac17b52056f8dc293a49c0bbf
Talha Zahid <italhazahid@gmail.com>
2025-12-14 13:00:15 +0500

    Added Reset Password

--- middlewares/email.js
+++ middlewares/email.js
@@ -1,5 +1,5 @@
 import transporter from "./emailConfig.js";
-import { Verification_Email_Template, Welcome_Email_Template } from "./EmailTemplate.js";
+import { Reset_Password_Template, Verification_Email_Template, Welcome_Email_Template } from "./EmailTemplate.js";
 
 export const sendVerificationEmail = async (toEmail, verificationCode) => {
     try {
@@ -22,6 +22,19 @@ export const sendWelcomeEmail = async (name, toEmail) => {
             subject: "Welcome to ChatX",
             text: `Welcome to ChatX! We're glad to have you on board.`, // plain‑text body
             html: Welcome_Email_Template.replace('{name}', name),
+        });
+    } catch (error) {
+        console.log("Error sending Email:", error)
+    }
+}
+export const sendResetPasswordEmail = async (toEmail, verificationCode) => {
+    try {
+        const info = await transporter.sendMail({
+            from: `"ChatX" <${process.env.EMAIL_USER}>`,
+            to: toEmail,
+            subject: "Reset Your Password",
+            text: `Use the following verification code to reset your password: ${verificationCode}`, // plain‑text body
+            html: Reset_Password_Template.replace('{verificationCode}', verificationCode),
         });
     } catch (error) {
         console.log("Error sending Email:", error)
