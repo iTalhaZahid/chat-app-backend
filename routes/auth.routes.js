f3fa8c44c916bd5e7dc21d3602b4be03cd61c871
Talha Zahid <italhazahid@gmail.com>
2025-12-14 11:46:26 +0500

    Addded Route and Nodemon Dep

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/auth.controllers.js";
+import { loginUser, registerUser, verifyEmail } from "../controllers/auth.controllers.js";
 
 const router= Router();
 
 router.post("/register",registerUser);
 router.post("/login",loginUser);
+router.post("/verify-email",verifyEmail);
 
 export default router;
